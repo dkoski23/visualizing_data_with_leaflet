ece3f970ff3551f766a21f608314d217edc9f7bf
dkoski23 <85533882+dkoski23@users.noreply.github.com>
2021-12-08 13:00:22 -0800

    Update logic.js
    Base layers are set up, and I'm now starting to work on the earthquakes.

--- static/js/logic.js
+++ static/js/logic.js
@@ -6,8 +6,31 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
     accessToken:API_KEY
 })
 
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}',{
+    attribution: 'Map data',
+    maxZoom: 18,
+    accessToken:API_KEY
+})
+
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}',{
+    attribution: 'Map data',
+    maxZoom: 18,
+    accessToken:API_KEY
+})
+
+
 let map = L.map('mapid',{
     center: [40.7, -94.5],
     zoom: 3,
     layers: [streets]
 })
+
+let baseMap = {
+    "Streets": streets,
+    "Satellite": satelliteStreets,
+    Light: light
+}
+
+let allEarthquakes = new L.LayerGroup()
+let largeEarthquakes = new L.LayerGroup()
+let tectonicplates = new L.LayerGroup()
