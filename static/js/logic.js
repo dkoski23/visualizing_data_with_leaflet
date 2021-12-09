119c7c09c86e64e36c5e917cda5fe1c55a55a956
dkoski23 <85533882+dkoski23@users.noreply.github.com>
2021-12-09 13:38:30 -0800

    Update logic.js
    Finally got the logic.js done! With instructor help, but this makes a lot of sense to me now.

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,36 +1,120 @@
 console.log("working");
 
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}',{
-    attribution: 'Map data',
+var start_map = L.tileLayer(
+  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+  {
+    attribution:
+      "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
     maxZoom: 18,
-    accessToken:API_KEY
-})
+    zoomOffset: -1,
+    id: "mapbox/light-v10",
+    accessToken: API_KEY
+  }
+);
 
-let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}',{
-    attribution: 'Map data',
-    maxZoom: 18,
-    accessToken:API_KEY
-})
+// We create the map object with options.
+var map = L.map("map", {
+  center: [
+    40.7, -94.5
+  ],
+  zoom: 3
+});
 
-let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}',{
-    attribution: 'Map data',
-    maxZoom: 18,
-    accessToken:API_KEY
-})
+// Add the tile layer to the map.
+start_map.addTo(map);
 
+// Get the earthquake geoJSON data and make a function to deal with the radius data, including addressing opacity and coloration.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
 
-let map = L.map('mapid',{
-    center: [40.7, -94.5],
-    zoom: 3,
-    layers: [streets]
-})
+  function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: getColor(feature.geometry.coordinates[2]),
+      color: "#000000",
+      radius: getRadius(feature.properties.mag),
+      stroke: true,
+      weight: 0.5
+    };
+  }
 
-let baseMap = {
-    "Streets": streets,
-    "Satellite": satelliteStreets,
-    Light: light
-}
+  // Make a function to determine the color based on magnitude. One could use whatever colors you want, but I am
+  // using darker colors for a larger magnitude
+  function getColor(depth) {
+    switch (true) {
+    case depth > 90:
+      return "#ea2c2c";
+    case depth > 70:
+      return "#ea822c";
+    case depth > 50:
+      return "#ee9c00";
+    case depth > 30:
+      return "#eecc00";
+    case depth > 10:
+      return "#d4ee00";
+    default:
+      return "#98ee00";
+    }
+  }
 
-let allEarthquakes = new L.LayerGroup()
-let largeEarthquakes = new L.LayerGroup()
-let tectonicplates = new L.LayerGroup()
+  // Figure out radius by magnitude and multipy it to be easier to see on the chart.
+  function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
+
+    return magnitude * 4;
+  }
+
+  // Add a GeoJSON layer to the map once the file is loaded.
+  L.geoJson(data, {
+    // We turn each feature into a circleMarker on the map.
+    pointToLayer: function(feature, latlng) {
+      return L.circleMarker(latlng);
+    },
+
+    style: styleInfo,
+    // Show location and magnitude
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup(
+        "Magnitude: "
+          + feature.properties.mag
+          + "<br>Depth: "
+          + feature.geometry.coordinates[2]
+          + "<br>Location: "
+          + feature.properties.place
+      );
+    }
+  }).addTo(map);
+
+  // Make a legend.
+  var legend = L.control({
+    position: "bottomright"
+  });
+
+  // Make the specifications for the legend
+  legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend");
+
+    var grades = [-10, 10, 30, 50, 70, 90];
+    var colors = [
+      "#98ee00",
+      "#d4ee00",
+      "#eecc00",
+      "#ee9c00",
+      "#ea822c",
+      "#ea2c2c"
+    ];
+
+    // Set coloration based on the grades in the last legend.onAdd function
+    for (var i = 0; i < grades.length; i++) {
+      div.innerHTML += "<i style='background: " + colors[i] + "'></i> "
+      + grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+    }
+    return div;
+  };
+
+  // Put the legend onto the map.
+  legend.addTo(map);
+});
